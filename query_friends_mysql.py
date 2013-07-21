aa64b3854146e43515c6a409c1c0802fe9020e2c
Joerg Baach <mail@baach.de>
2013-07-21 13:19:52 +0200

    added mysql query scripts

---
+++ query_friends_mysql.py
@@ -0,0 +1,52 @@
+import MySQLdb
+import cPickle, time, uuid, sys,random
+db = MySQLdb.connect("localhost","friends","","friends" )
+
+pathlength,repeats = sys.argv[1:3]
+pathlength=int(pathlength)
+repeats = int(repeats)
+# "match  p=n:node<-[*4..4]-m where n.noscenda_name='dbpedia:Berlin' return count(p);"
+
+def make_sql(l,target):
+    if l<1:
+        raise 'len too small'
+    start = """
+    select 
+        count(*)"""
+    froms = """
+    from
+        t_user,
+        t_user_friend as uf1"""
+
+    wheres = """
+    where
+        t_user.name='%s' and 
+        t_user.id = uf1.user_1""" % target
+    i = 1
+    if l>1:
+        for i in range(2,l+1):
+            froms+=', \n\tt_user_friend as uf%s' % i
+            wheres+=' and\n\tuf%s.user_2 = uf%s.user_1' % (i-1,i)
+    #froms+=""",
+    #    t_user as final"""
+    #wheres+=""" and
+    #    uf%s.user_2=final.id""" % i
+    return start+froms+wheres+';'
+
+
+
+for i in range(0,repeats):
+    print 'run %s' % i
+    c = db.cursor()
+    target = 'person%s' % random.randint(1,10001)
+    sql = make_sql(pathlength,target)
+    print sql
+    print
+    start = time.time()
+    c.execute(sql)
+    print time.time()-start
+    rows = c.fetchall()
+    print 'Num of paths: ',len(rows)
+    for row in rows:
+        #print str(row).decode('utf-8')
+        pass
---
+++ query_friends_mysql_python.py
@@ -0,0 +1,25 @@
+import MySQLdb
+import cPickle, time, uuid, sys, random
+db = MySQLdb.connect("localhost","friends","","friends" )
+
+pathlength,repeats = sys.argv[1:3]
+pathlength=int(pathlength)
+repeats = int(repeats)
+
+for i in range(0,repeats):
+    print 'run %s' % i
+    target = 'person%s' % random.randint(1,10001)
+    c = db.cursor()
+    start = time.time()
+    c.execute("select id from t_user where name=%s",(target,))
+    id = c.fetchone()[0]
+    ids = set([str(id)])
+    for i in range(0,pathlength):
+        format_strings = ','.join(['%s' for i in range(0,len(ids))])
+        sql = 'select user_2 from t_user_friend where user_1 in (%s)' % format_strings
+        c.execute(sql,tuple(ids))
+        ids = set()
+        for row in c.fetchall():
+            ids.add(str(int(row[0])))
+    print time.time()-start
+    print len(ids)
