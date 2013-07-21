6248709096a5c4e29703905a0fa3007fc60f613a
Joerg Baach <mail@baach.de>
2013-07-21 15:56:43 +0200

    query script for neo4j, neo4j-wrapper.conf, updated neo4j.properties

---
+++ query_friends_neo4j.py
@@ -0,0 +1,22 @@
+import cPickle, neo4jconnector,simplejson, sys,time, random
+
+if len(sys.argv) < 3:
+    print 'usage:%s number_of_hops repetitions' % sys.argv[0]
+    sys.exit()
+
+pathlength,repeats = sys.argv[1:3]
+pathlength=int(pathlength)
+repeats = int(repeats)
+# "match  p=n:node<-[*4..4]-m where n.noscenda_name='dbpedia:Berlin' return count(p);"
+
+g = neo4jconnector.BaseNeo4jConnector(debug=0)
+
+for i in range(0,repeats):
+    print 'run %s' % i
+    target = 'person%s' % random.randint(1,10001)
+    start = time.time()
+    result = g.queryd([['match n:node-[r*%s..%s]->m:node where n.noscenda_name={target} return count(r);' % (pathlength,pathlength),
+                       dict(target=target)]])
+    print time.time()-start
+    print target, 
+    print result[0]['count(r)']
