29e512df616ef33ddbf34eee39c2171ac76309bb
Joerg Baach <mail@baach.de>
2013-07-22 14:22:07 +0200

    added python-embedded/jpype based import script

---
+++ njfriends_import.py
@@ -0,0 +1,40 @@
+import os,jpype,time,random, cPickle
+os.environ['NEO4J_PYTHON_JVMARGS'] = '-Xms4G -Xmx8G -XX:MaxPermSize=1G'
+os.environ['JAVA_HOME']='/usr/lib/jvm/jdk1.7.0/'
+
+from neo4j import GraphDatabase
+db = GraphDatabase('data')
+
+print 'deleting'
+with db.transaction:
+    db.query('start r=relationship(*) delete r');
+    db.query('start n=node(*) delete n;')
+
+print 'reading'
+friends = cPickle.load(open('friends.pickle'))
+
+print 'nodes'
+nodes = {}
+with db.transaction:
+    for a,data in friends.items():
+        n = db.node(noscenda_name='person%s'%a)
+        nodes[a]=n
+        if a%1000==0:
+            print 'n ',a
+
+print 'relations'
+frienditems = friends.items()
+i = 1
+p = 0
+while p<len(frienditems):
+    items = frienditems[p:p+1000000]
+    with db.transaction:
+        print 'transaction relations: ',p
+        for a,targets in items:
+            for b in targets:
+                i+=1
+                getattr(nodes[a],'friend')(nodes[b])
+                if i%10000==0:
+                    print 'r ',i
+    p+=1000000
+print 'fini'
